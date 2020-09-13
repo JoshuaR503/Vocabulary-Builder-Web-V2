c8461397f8dd7954d1c107ba6f4ce6706962bda9
Joshua Garcia <josshuagarciia000@gmail.com>
2020-09-13 11:36:52 -0700

    Cleaned source code.

--- js/game.js
+++ js/game.js
@@ -1,73 +1,34 @@
+// Repeated elements.
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-
 const images  = Array.from(document.getElementsByClassName('image'));
 
+// DOM elements.
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const loader = document.getElementById('loader');
 const game = document.getElementById('game');
 
+// Constants.
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 10;
+
+// Variables that hold state.
 let score = 0;
 let questionCounter = 0;
 let acceptingAnswers = false;
 
 let currentQuestion = {};
 let availableQuesions = [];
-
 let questions = [];
-scoreText.innerText = '';
 
-fetch('../assets/questions.json')
-.then((res) =>  res.json())
-.then((loadedQuestions) => {
-
-    questions = loadedQuestions.map((loadedQuestion) => {
-
-        const formattedQuestion = {
-            question: loadedQuestion.question,
-        };
-
-        const answerChoices = [...loadedQuestion.incorrect_answers];
-
-        formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-        
-        answerChoices.splice(
-            formattedQuestion.answer - 1,
-            0,
-            loadedQuestion.correct_answer
-        );
-
-        answerChoices.forEach((choice, index) =>  formattedQuestion['choice' + (index + 1)] = choice);
-        answerChoices.sort((a, b) =>  a.length - b.length);
-
-
-        return formattedQuestion;
-    });
-
-    startGame();
-})
-.catch((err) =>  console.error(err));
-
-//CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 10;
-
-const startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuesions = [...questions];
-
-    loadNewQuestion();
-    game.classList.remove('hidden');
-    loader.classList.add('hidden');
-};
-
+// Game loop.
 const loadNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+
         localStorage.setItem('mostRecentScore', score);
         localStorage.setItem('possibleScore',MAX_QUESTIONS * 10 );
-        //go to the end page
-        return window.location.assign('/end.html');
+
+        return window.location.assign('/pages/end.html');
     }
 
     // Increment counter.
@@ -85,7 +46,9 @@ const loadNewQuestion = () => {
     acceptingAnswers = true;
 };
 
-choices.forEach((choice) => {
+const incrementScore = (num) =>  scoreText.innerText = score += num;
+
+const hookEventLister = ( choice ) => {
     choice.addEventListener('click', (e) => {
         if (!acceptingAnswers) {
             return;
@@ -94,8 +57,7 @@ choices.forEach((choice) => {
         acceptingAnswers = false;
 
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-        const classToApply = selectedAnswer == currentQuestion.answer 
+        const classToApply = selectedChoice.dataset['number'] == currentQuestion.answer 
             ? 'correct' 
             : 'incorrect';
 
@@ -108,12 +70,52 @@ choices.forEach((choice) => {
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             loadNewQuestion();
-        }, 500);
+        }, 250);
     });
-});
+}
 
+const hookEventListeners = () => choices.forEach((choice) => hookEventLister(choice));
 
-incrementScore = (num) => {
-    score += num;
-    scoreText.innerText = score;
+/// Load and start game.
+const startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+
+    loadNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
+
+const loadQuestions = (data) => {
+    questions = data.map((loadedQuestion) => {
+    
+        const choices = [...loadedQuestion.incorrect_answers];
+        const question = {
+            question: loadedQuestion.question,
+            answer: Math.floor(Math.random() * 3) + 1
+        };
+        
+        const answer = question.answer - 1;
+
+        choices.splice(answer, 0, loadedQuestion.correct_answer);
+        choices.forEach((choice, i) => question['choice' + (i + 1)] = choice);
+        choices.sort((a, b) =>  a.length - b.length);
+
+        return question;
+    });
+}
+
+const fetchContent = () => {
+    fetch('../assets/questions.json')
+    .then((response) => response.json())
+    .then((data) => loadQuestions(data))
+    .then((_) => startGame())
+    .catch((err) => console.error(err));
+}
+
+/// Program starts here.
+(() => {
+    fetchContent();
+    hookEventListeners();
+})();
