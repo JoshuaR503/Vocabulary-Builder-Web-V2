a60a44626b74895be394b3356fd420b9c0d90406
Joshua Garcia <josshuagarciia000@gmail.com>
2020-09-14 13:17:16 -0700

    Program working.

--- v2/src/components/pages/Game.vue
+++ v2/src/components/pages/Game.vue
@@ -3,60 +3,173 @@
 
     <!-- <div id="loader"></div> -->
       
-    <div id="game" class="justify-center flex-column">
+    <div class="game justify-center flex-column">
 
-      <div id="hud">
-        <div id="hud-item">
+      <div class="hud">
+        <div class="hud-item">
           <h1 class="hud-main-text"> {{score}} </h1>
         </div>
       </div>
 
-      <div class="imageRow">
+      <code>{{question}}</code>
 
-        
-        <img width="75" height="75"  class="animate__animated animate__fadeInDown image">
-        <img width="75" height="75"  class="animate__animated animate__fadeInDown image">
-        <img width="75" height="75"  class="animate__animated animate__fadeInDown image">  
+      <div class="imageRow">
+        <img v-bind:src="'/assets' + question.question" width="75" height="75" class="animate__animated animate__fadeInDown image">
+        <img v-bind:src="'/assets' + question.question" width="75" height="75" class="animate__animated animate__fadeInDown image">
+        <img v-bind:src="'/assets' + question.question" width="75" height="75" class="animate__animated animate__fadeInDown image">  
+        <img v-bind:src="'/assets' + question.question" width="75" height="75" class="animate__animated animate__fadeInDown image">  
       </div>
 
-      <!-- <br>
-      <br>
-              <code> {{ currentQuestion.choices }} </code>  -->
+       <div 
+          v-for="(choice, index) in question.choices" 
+          v-bind:key="index"
 
-
-      <div 
-        v-for="(choice, index) in currentQuestion.choices" 
-        v-bind:key="index" 
-        class="animate__animated animate__fadeInUp choice-container">
-
+          @click="checkAnswer($event, index + 1)"
+          class="animate__animated animate__fadeInUp choice-container">
         <p class="choice-text">{{choice}}</p>
       </div>
-
     </div>
 
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions, mapMutations } from 'vuex';
 
 export default {
   name: 'Game',
   computed: mapGetters([
-    'loading', 
     'score', 
     'acceptingAnswers', 
-    'currentQuestion',
+    'question',
     'availableQuestions',
-    'questions'
   ]),
   methods: {
+    ...mapMutations(['setScore']),
     ...mapActions(['loadQuestions', 'loadQuestion']),
+
+    checkAnswer(event, choice) {
+
+      if (this.acceptingAnswers) {
+
+        const classToApply = this.question.answer === choice
+          ? 'correct' 
+          : 'incorrect';
+
+        const score = classToApply === 'correct' ? 10 : -10;
+        const selectedChoice =  event.target.parentElement;
+
+        this.$store.commit('setAcceptingAnswers', false);
+        this.$store.commit('setScore', score);
+
+        selectedChoice.classList.add(classToApply);
+
+        setTimeout(() => {
+          selectedChoice.classList.remove(classToApply);
+          this.loadQuestion();
+        }, 250);
+      }
+    },
   },
-  created() {
-    this.loadQuestions();
+  async created () {
+    await this.loadQuestions();
     this.loadQuestion();
   },
-
 }
 </script>
+
+<style scoped>
+code {
+  font-family: Consolas,"courier new";
+  color: crimson;
+  background-color: #f1f1f1;
+  padding: 2px;
+  font-size: 2rem;
+}
+
+.choice-container {
+  display: flex;
+  margin-bottom: 0.8rem;
+  width: 100%;
+  font-size: 1.8rem;
+  /* border: 0.2rem solid rgba(80, 80, 80, 0.25); */
+  background-color: #f5f5f5;
+}
+
+.choice-container:hover {
+  cursor: pointer;
+  /* box-shadow: 0 0.4rem 1.4rem 0  rgba(49, 49, 49, 0.25); */
+  transform: translateY(-0.1rem);
+  transition: transform 150ms;
+}
+.imageRow {
+  white-space: nowrap;
+}
+.report-emoji{
+  margin-top:4rem;
+  margin-bottom:4rem;
+  font-size: 2rem;
+  font-weight: 500;
+  letter-spacing: .05rem;
+
+  color: #181818;
+  padding: 1.5rem;
+  width: 100%;
+}
+
+.choice-text {
+  font-size: 2.5rem;
+  font-weight: 600;
+  letter-spacing: .05rem;
+
+  color: #181818;
+  padding: 1.5rem;
+  width: 100%;
+}
+
+.correct {
+  background-color: #4bd16ace;
+}
+
+.incorrect {
+  background-color: #f14f5fde;
+}
+
+/* HUD */
+
+.hud {
+  margin-bottom: 0.5rem;
+  display: flex;
+  justify-content: space-between;
+}
+
+.hud-prefix {
+  text-align: left;
+  font-size: 2rem;
+}
+
+.hud-main-text {
+  text-align: left;
+}
+
+
+/* LOADER */
+.loader {
+  border: 1.6rem solid white;
+  border-radius: 50%;
+  border-top: 1.6rem solid #56a5eb;
+  width: 12rem;
+  height: 12rem;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
--- v2/src/store/modules/questions.js
+++ v2/src/store/modules/questions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const state = {
     loading: true,
 
@@ -5,10 +7,9 @@ const state = {
     questionCounter: 0,
 
     acceptingAnswers: false,
-    currentQuestion: {},
-
     availableQuestions: [],
-    questions: []
+    questions: [],
+    question: {},
 };
 
 const getters = {
@@ -18,10 +19,10 @@ const getters = {
     questionCounter: state => state.questionCounter,
 
     acceptingAnswers: state => state.acceptingAnswers,
-    currentQuestion: state => state.currentQuestion,
 
     availableQuestions: state => state.availableQuestions,
     questions: state => state.questions,
+    question: state => state.question,
 };
 
 const mutations  = {
@@ -30,55 +31,21 @@ const mutations  = {
     setScore: (state, score) => state.score = state.score += score,
     incrementQuestionCounter: (state) => state.questionCounter++,
 
-    setAcceptingAnswers: (state, data) => state.data = data,
-
-    setCurrentQuestion: (state, question) => state.currentQuestion = question,
-    setAvilableQuestions: (state, question) => state.availableQuestions = question,
-    removeQuestion: (state, questionIndex) => state.availableQuesions.splice(questionIndex, 1),
+    setAcceptingAnswers: (state, data) => state.acceptingAnswers = data,
     setQuestions: (state, questions) => state.questions = questions,
+    setQuestion: (state, question) => state.question = question,
+
+    setAvilableQuestions: (state, questions) => state.availableQuestions = questions,
+
+    removeQuestion: (state, questionIndex) => state.availableQuestions.splice(questionIndex, 1),
 }
 
 const actions = {
-
-    loadQuestion({commit, state}) {
-
-        const MAX_QUESTIONS = 10;
-        const gameOver = 
-            state.availableQuestions.length === 0 || 
-            state.questionCounter >= MAX_QUESTIONS;
-
-        if (gameOver) {
-
-            // localStorage.setItem('mostRecentScore', score);
-            // localStorage.setItem('possibleScore',MAX_QUESTIONS * 10 );
-    
-            // return window.location.assign('/pages/end.html');
-        } else {
-
-            // Select random question.
-            const questionIndex = Math.floor(Math.random() * state.availableQuestions.length);
-
-            console.log( state.availableQuestions[questionIndex]);
-
-            // Increment counter.
-            commit('incrementQuestionCounter');
-            // Set current question to question selected.
-            commit('setCurrentQuestion', state.availableQuestions[questionIndex]);
-            // Remove question form index.
-            commit('removeQuestion', questionIndex);
-            // Start accepting questions again.
-            commit('setAcceptingAnswers', true)
-        }
-    },
-
-    loadQuestions({commit}) {
-        commit('setLoading', true);
-
-        fetch('/assets/questions.json')
-        .then((response) => response.json())
-        .then((data) => {
-
-            const questions = data.map((loadedQuestion) => {
+    async loadQuestions({commit}) {
+        await axios
+        .get('/assets/questions.json')
+        .then((response) => {
+            const questions = response.data.map((loadedQuestion) => {
             
                 const randomNumber = Math.floor(Math.random() * 3) + 1;
                 const choices = [...loadedQuestion.incorrect_answers];
@@ -94,19 +61,39 @@ const actions = {
                 choices.splice(answer, 0, loadedQuestion.correct_answer);
                 choices.forEach((choice) => question['choices'].push(choice));
 
-                // choices.sort((a, b) =>  a.length - b.length);
-        
                 return question;
             });
 
-            console.log(questions[0]);
-    
-            commit('setScore', 0);
             commit('setQuestions', questions);
             commit('setAvilableQuestions', [...questions]);
-            commit('setLoading', false);
         })
-        .catch((err) => console.error(err));
+        .catch((err) => console.error('There was an error', err));
+    },
+
+    loadQuestion({commit, state}) {
+        const MAX_QUESTIONS = 10;
+        const gameOver = 
+            state.availableQuestions.length === 0 || 
+            state.questionCounter >= MAX_QUESTIONS;
+
+        if (gameOver) {
+
+            localStorage.setItem('mostRecentScore', state.score);
+            localStorage.setItem('possibleScore',MAX_QUESTIONS * 10 );
+    
+            // return window.location.assign('/pages/end.html');
+        } else {
+            // Select random question.
+            const questionIndex = Math.floor(Math.random() * state.availableQuestions.length);
+            const question = state.availableQuestions[questionIndex];
+            
+            // Set current question to question selected.
+            commit('setQuestion', question);
+            // Remove question form index.
+            commit('removeQuestion', questionIndex);
+            // Start accepting questions again.
+            commit('setAcceptingAnswers', true);
+        }
     },
 }
 
